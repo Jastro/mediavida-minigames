fdd77c52f6d8a536f1a5f6b053692b683f1123c5
Ridote <daniridao@gmail.com>
2025-09-20 20:58:30 +0100

    adding rocks and spikes

---
+++ minigames/TheOrcsAreComingFromTheEast/environment/spikes/spikes.gd
@@ -0,0 +1,28 @@
+extends AnimatedSprite2D
+
+@export var time_up = 1
+@export var time_down = 1
+
+const ENEMY_MASK : int = 4
+
+var up = false
+var timer
+
+func _ready():
+	$Hitbox.set_new_layer(ENEMY_MASK)
+	timer			= Timer.new()
+	timer.one_shot	= true
+	add_child(timer)
+	timer.timeout.connect(_on_timeout)
+	timer.start(time_down)
+
+func _on_timeout():
+	if(up):
+		play_backwards("default")
+		timer.start(time_down)
+	else:
+		play("default")
+		await get_tree().create_timer(0.4).timeout
+		$Hitbox.enable(time_up)
+		timer.start(time_up)
+	up = !up
